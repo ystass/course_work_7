95063d417831314479d5dea26c0944a4f7192bff
ystass <stepanischevyy@mail.ru>
2024-10-07 17:54:25 +0400

    установка и настройки djangorestframework-simplejwt для авторизации через JWT

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
