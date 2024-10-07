6a696b9054c46a6489ce42e790155d364145b37a
ystass <stepanischevyy@mail.ru>
2024-10-07 16:04:22 +0400

    csu

---
+++ users/management/commands/csu.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(email="admin@admin.com",)
+        user.set_password('qwe123asd456')
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
