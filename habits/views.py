5056c6269448271d6a3c7023a612273ca4f38845
ystass <stepanischevyy@mail.ru>
2024-10-08 16:25:15 +0400

    фильтрация

--- habits/views.py
+++ habits/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from habits.pagination import HabitPaginator
@@ -10,6 +11,9 @@ class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwner]
     pagination_class = HabitPaginator
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ("action",)
+    ordering_fields = ("time",)
 
     def perform_create(self, serializer):
         new_habit = serializer.save()
