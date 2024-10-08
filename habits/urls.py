c1b2b32a9600b87a315ac047a91d57e00170d525
ystass <stepanischevyy@mail.ru>
2024-10-08 16:32:39 +0400

    urls

--- habits/urls.py
+++ habits/urls.py
@@ -1,9 +1,14 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from habits.apps import HabitsConfig
-from habits.views import HabitViewSet
+from habits.views import HabitViewSet, PublicHabitListAPIView
 
 app_name = HabitsConfig.name
 
 router = SimpleRouter()
 router.register("", HabitViewSet, basename="habits")
+
+urlpatterns = [
+    path("public/", PublicHabitListAPIView.as_view(), name="public"),
+] + router.urls
