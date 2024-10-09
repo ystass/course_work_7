07f47b417fa82d2a4929c2caf1733b69038f4569
ystass <stepanischevyy@mail.ru>
2024-10-09 15:54:49 +0400

    задача для отправки сообщения в телеграм

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,6 +1,36 @@
 from celery import shared_task
+from django.utils import timezone
+
+from habits.models import Habit
+from habits.services import send_telegram_massage
 
 
 @shared_task()
 def habit():
-    pass
+    now = timezone.now()
+    print(f"Время: {now}")
+
+    habits = Habit.objects.filter(
+        time__lte=now, time__gt=now - timezone.timedelta(minutes=1)
+    )
+
+    print(f"Количество привычек: {habits.count()}")
+
+    for habit_item in habits:
+        if habit_item.user.tg_chat_id:
+            send_telegram_massage(habit_item)
+            if habit_item.frequency_unit == "days":
+                habit_item.time = habit_item.time + timezone.timedelta(
+                    days=habit_item.frequency_number
+                )
+            elif habit_item.frequency_unit == "hours":
+                habit_item.time = habit_item.time + timezone.timedelta(
+                    hours=habit_item.frequency_number
+                )
+            elif habit_item.frequency_unit == "minutes":
+                habit_item.time = habit_item.time + timezone.timedelta(
+                    minutes=habit_item.frequency_number
+                )
+            habit_item.save()
+        else:
+            print(f"У пользователя {habit_item.user} не указан tg-id")
